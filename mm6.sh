5e9d3abd28e564131209d9862960f3aafdae936a
xiaoping <cupider88@126.com>
2017-12-26 18:55:11 +0800

    opendl for android6

--- mm6.sh
+++ mm6.sh
@@ -7,7 +7,7 @@ mmm src/Media/Codec/base6
 echo -e 'build codec done..........................\n'
 
 
-plib=../../../../out/target/product/wing-k70/obj/lib
+plib=../../../../out/target/product/generic/obj/lib
 cp $plib/libCodecBase.so src/bin/libCodecBase6.so
 cp $plib/libgreat_media.so src/bin
 echo -e 'copy libs done...........................\n'
