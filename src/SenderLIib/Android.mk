b354e15f5a7be19d23b70d91109e5be6d241cad5
great <cupider88@126.com>
2018-02-10 11:45:45 +0800

    add sendermedia

---
+++ src/SenderLIib/Android.mk
@@ -0,0 +1,51 @@
+LOCAL_PATH:= $(call my-dir)
+
+
+##################################################
+###                 great media                ###
+##################################################
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    	$(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/*.c*)) \
+	$(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/../RtpTrans/*.c*)) \
+    	$(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/../RtpTrans/rtplib/jrtplib/*.c*)) \
+	$(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/../Common/*.c*))
+
+	#JCrypto rely libbinder libmedia
+	#libgui:need for android6 camera graphic buffer
+LOCAL_SHARED_LIBRARIES := \
+						libandroid_runtime \
+						libnativehelper \
+						liblog \
+						libdl
+
+
+LOCAL_C_INCLUDES += \
+    frameworks/base/core/jni \
+	$(LOCAL_PATH)/../Common \
+	$(LOCAL_PATH)/../RtpTrans	\
+	$(LOCAL_PATH)/../RtpTrans/rtplib/jrtplib	\
+    $(call include-path-for, libhardware)/hardware \
+    $(call include-path-for, corecg graphics)
+
+	LOCAL_C_INCLUDES += external/stlport/stlport bionic
+
+LOCAL_CFLAGS +=
+
+#need for android4 compile, no for android6
+#LOCAL_SHARED_LIBRARIES += libstlport 
+
+
+LOCAL_MODULE:= libgreat_sender
+
+
+include $(BUILD_SHARED_LIBRARY)
+
+LOCAL_PRELINK_MODULE:= false
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+
+
+
