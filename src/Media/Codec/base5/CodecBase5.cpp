0711b9a508633e7744799b98f46867cf57a0336a
great <cupider88@126.com>
2018-02-10 08:51:23 +0800

    android5 codec base

--- src/Media/Codec/base5/CodecBase5.cpp
+++ src/Media/Codec/base5/CodecBase5.cpp
@@ -4,7 +4,9 @@
 #include "media_MediaCodec.h"
 
 #include "Configure.h"
-#define TAG "CodecBase"
+
+#include "ComDefine.h"
+#define TAG "CodecBase5"
 
 namespace android 
 {
@@ -13,13 +15,13 @@ status_t CodecBase::CreateCodec(const sp<AMessage> &format, const sp<Surface> &s
 {
 	status_t err;
 
-	ALOGTEST("function %s,line:%d configure...1", __FUNCTION__, __LINE__);
+	GLOGD("function %s,line:%d configure...1", __FUNCTION__, __LINE__);
 	
 	sp<IGraphicBufferProducer> bufferProducer;
 	if(surface!=NULL)
 		bufferProducer = surface->getIGraphicBufferProducer();
 	err = mCodec->configure(format, bufferProducer, crypto, flags);
-	ALOGTEST("function %s,line:%d configure...2 err:%d", __FUNCTION__, __LINE__, err);
+	GLOGD("function %s,line:%d configure...2 err:%d", __FUNCTION__, __LINE__, err);
 
 	return err;
 }
